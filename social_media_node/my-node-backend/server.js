f5c357c3c9d6d7c92406ad4e1edc5f55b684fb34
Mark John <markjohnjopia1@gmail.com>
2024-09-03 04:38:14 +0800

    Setting Up MongoDB

--- social_media_node/my-node-backend/server.js
+++ social_media_node/my-node-backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
 app.use(cors()); // Enable CORS
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
@@ -9,3 +10,11 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+mongoose.connect('mongodb://localhost:27017/mydb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => {
+    console.log('Connected to MongoDB');
+  }).catch((error) => {
+    console.error('MongoDB connection error:', error);
+  });
