c6075083c3e66dc620ad008b7c23721ea10e1667
Mark John <markjohnjopia1@gmail.com>
2024-09-03 01:32:48 +0800

    Angular Frontend AuthModule

---
+++ social_media_node/back-end/auth/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+  LoginComponent,
+  RegisterComponent
+  ],
+  imports: [
+  CommonModule
+  ],
+  exports : [
+  LoginComponent,
+  RegisterComponent
+  ]
+  })
+  export class AuthModule { }
