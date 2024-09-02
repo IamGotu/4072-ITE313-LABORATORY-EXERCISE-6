0f71b4ed310e061d953b62248d4f79620f133634
Mark John <markjohnjopia1@gmail.com>
2024-09-02 21:16:23 +0800

    minor changes

--- codeigniter/app/Views/templates/header.php
+++ codeigniter/app/Views/templates/header.php
@@ -13,7 +13,7 @@
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container">
                     <div class="container-fluid">
-                        <a class="navbar-brand" href="#">App</a>
+                        <a class="navbar-brand" href="#">Codeigniter</a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                         </button>
@@ -23,7 +23,7 @@
                             <a class="nav-link" href="/">Login</a>
                             </li>
                             <li class="nav-item">
-                            <a class="nav-link" href="/register">Register</a>
+                            <a class="nav-link" href="../register">Register</a>
                             </li>
                         </ul>
                         </div>
