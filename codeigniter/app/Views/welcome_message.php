63eb5c504dff4fb582ea04b05f5ea0e9d6f157de
Mark John <markjohnjopia1@gmail.com>
2024-09-04 00:32:30 +0800

    adding logout button for users to logout

--- codeigniter/app/Views/welcome_message.php
+++ codeigniter/app/Views/welcome_message.php
@@ -216,10 +216,7 @@
             <li class="menu-item hidden"><a href="#">Home</a></li>
             <li class="menu-item hidden"><a href="https://codeigniter.com/user_guide/" target="_blank">Docs</a>
             </li>
-            <li class="menu-item hidden"><a href="https://forum.codeigniter.com/" target="_blank">Community</a></li>
-            <li class="menu-item hidden"><a
-                    href="https://codeigniter.com/contribute" target="_blank">Contribute</a>
-            </li>
+            <li class="menu-item hidden"><a href="<?= base_url('/logout') ?>" target="_blank">Logout</a></li>
         </ul>
     </div>
 
