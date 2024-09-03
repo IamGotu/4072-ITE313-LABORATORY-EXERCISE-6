1bdd9ac4fab889b820434df67d16f128c31f9ada
Mark John <markjohnjopia1@gmail.com>
2024-09-03 09:43:58 +0800

    Creating a Model anImplementing CRUD Operations in Angulard Routes in Express

--- social_media_node/my-node-backend/server.js
+++ social_media_node/my-node-backend/server.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const taskRoutes = require('./routes/tasks');
 const app = express();
 const port = 3000;
-const mongoose = require('mongoose');
-app.use(cors()); // Enable CORS
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+mongoose.connect('mongodb://localhost:27017/mydb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('MongoDB connection error:', error);
 });
+app.use(cors());
+app.use(express.json());
+app.use('/tasks', taskRoutes);
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-mongoose.connect('mongodb://localhost:27017/mydb', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }).then(() => {
-    console.log('Connected to MongoDB');
-  }).catch((error) => {
-    console.error('MongoDB connection error:', error);
-  });
