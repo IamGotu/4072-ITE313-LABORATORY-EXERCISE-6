6175f788953caca800ee71d8b881b929c58a2b1d
Mark John <markjohnjopia1@gmail.com>
2024-09-03 09:49:01 +0800

    Implementing Authentication in Angular

--- social_media_node/my-node-backend/server.js
+++ social_media_node/my-node-backend/server.js
@@ -18,3 +18,7 @@ app.use('/tasks', taskRoutes);
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
