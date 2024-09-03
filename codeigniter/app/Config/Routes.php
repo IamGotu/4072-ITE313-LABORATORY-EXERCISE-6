e87c26e14bbb6222d8186f20bb5017a61b2b38ec
Mark John <markjohnjopia1@gmail.com>
2024-09-04 00:29:03 +0800

    set up router for redirection and functions

--- codeigniter/app/Config/Routes.php
+++ codeigniter/app/Config/Routes.php
@@ -5,4 +5,15 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Home::index'); //route home page
+
+$routes->get('/register', 'User::register'); //route to register page
+
+$routes->get('/login', 'User::login'); //route to login page
+
+$routes->get('/logout', 'User::logout'); //route to logout
+
+$routes->post('/store', 'User::store'); //route to store data in database
+
+$routes->post('/verifylogin', 'User::verifylogin'); //route to verify login
+
