e5b3a9034741974dfa95ca3d0a5b6d59ce77e377
Mark John <markjohnjopia1@gmail.com>
2024-09-16 11:26:57 +0800

    Post model with migration file that define the necessary fields for a post

--- social_media_app/app/Models/Post.php
+++ social_media_app/app/Models/Post.php
@@ -7,5 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+        use HasFactory;
+
+        public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
--- social_media_app/database/migrations/2024_09_13_125901_create_posts_table.php
+++ social_media_app/database/migrations/2024_09_13_125901_create_posts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->text('content');
+            $table->integer('likes_count')->default(0);
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
