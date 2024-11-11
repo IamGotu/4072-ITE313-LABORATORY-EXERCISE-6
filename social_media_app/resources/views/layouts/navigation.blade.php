e2e017e3ffe63235aed2df3e04c076267d3060be
Mark John <markjohnjopia1@gmail.com>
2024-11-11 22:08:45 +0800

    complete name of user in the navigation bar

--- social_media_app/resources/views/layouts/navigation.blade.php
+++ social_media_app/resources/views/layouts/navigation.blade.php
@@ -99,16 +99,7 @@
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-300">
             <div class="px-4">
-                <div class="font-medium text-base text-gray-800">
-                    {{ Auth::user()->first_name }}
-                    @if (Auth::user()->middle_name)
-                        {{ ' ' . Auth::user()->middle_name }}
-                    @endif
-                    {{ ' ' . Auth::user()->last_name }}
-                    @if (Auth::user()->suffix)
-                        {{ ' ' . Auth::user()->suffix }}
-                    @endif
-                </div>
+                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
             </div>
 
